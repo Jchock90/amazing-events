518dbab9b83afab4e52ebb68603c5e9b78696141
Jesus <jchockmansilla@gmail.com>
2023-02-27 00:59:33 -0300

    version2

---
+++ assets/js/pastevents.js
@@ -0,0 +1,16 @@
+let htmlEvents = "";
+for (let event of data.events) {
+    let currentDate = new Date(data.currentDate);
+    let eventDate = new Date(event.date);
+    if (eventDate < currentDate) {
+        htmlEvents += `<div class="card" style="width: 16rem; height: 22rem;">
+        <div class="card-body">
+        <img src="${event.image}" class="card-img-top" alt="imageevent" style="object-fit: cover; width: 220px; height: 100px;">
+        <h3 class="">${event.name}</h3>
+        <p class="">${event.description}</p>
+        <span>Price: $${event.price}</span><a href="./details.html" class="btn btn-primary btn-sm">Details</a>
+        </div>
+        </div>`;
+    }
+}
+document.querySelector('div.row').innerHTML += htmlEvents;
